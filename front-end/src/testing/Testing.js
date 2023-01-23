58db79e62e9a6443e8ab1e274fc1ed3e150ba854
Kaj <tassi100@mail.chapman.edu>
2023-01-23 15:46:25 -0800

    adding nav

--- front-end/src/testing/Testing.js
+++ front-end/src/testing/Testing.js
@@ -1,14 +1,40 @@
 import React from "react";
 
+import "./Testing.css";
+
 export default function Testing() {
 
 
     
     return (
         <>
-            <div className="headingBar d-md-flex my-3 p-2">
-                <h1>Hello World</h1>
-            </div>
+        <body>  
+            <header id="header" class="fixed-top">
+                <div class="container d-flex align-items-center justify-content-lg-between">
+                    <h1 class="logo me-auto me-lg-0">GP</h1>
+                    <nav id="navbar" class="navbar order-last order-lg-0">
+                        <ul>
+                            <li>
+                                <a class="nav-link scrollto active" href="hero">Home</a>
+                            </li>
+                            <li>
+                                <a class="nav-link scrollto" href="about">About</a>
+                            </li>
+                            <li>
+                                <a class="nav-link scrollto" href="services">Services</a>
+                            </li>
+                            <li>
+                                <a class="nav-link scrollto" href="portfolio">Port</a>
+                            </li>
+                            <li>
+                                <a class="nav-link scrollto" href="#contact">Contact</a>
+                            </li>
+                        </ul> 
+                        <i class="bi bi-list mobile-nav-toggle"></i>
+                    </nav>
+                </div>
+            </header>
+        </body>  
         </>
     );
 }
